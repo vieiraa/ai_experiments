eaadc7eed60de51f0e8d17dae9e7c7384a78dc4d
Jordy Vieira <jordyvieira@hotmail.com>
2020-04-29 15:31:18 -0300

    Updates

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from sklearn.model_selection import train_test_split
 import gc
 import time
 
-max_14b = (2 ** 14) - 1
-max_16b = (2 ** 16) - 1
+max_14b = float((2 ** 14) - 1)
+max_16b = float((2 ** 16) - 1)
 
 def crop_bg(img, gauss=True):
     normm = normalize(img, True)
@@ -46,7 +46,7 @@ def invert(img, max_val=1.0):
 
     return ones - img
 
-def show_img(img, method='pillow', norm=False):
+def show_img(img, method='cv2', norm=False):
     if norm:
         img = normalize(img)
     
@@ -113,11 +113,10 @@ def load_img(path, format='.png', is_mask=False):
     for root, _, files in os.walk(path):
         for f in files:
             if f.find(format) != -1:
-                scans.append(np.array(Image.open(os.path.join(root, f))))
+                scans.append(np.array(cv2.imread(os.path.join(root, f), -1)))
 
     scans = np.array(scans)
     scans = np.reshape(scans, scans.shape + (1,))
-    scans = normalize(scans, max_14b) if not is_mask else normalize(scans, max_16b)
 
     return scans
 
@@ -130,17 +129,17 @@ def train_generator(images, masks, batch_size, aug_dict, image_color_mode="grays
 
     image_generator = image_datagen.flow(
         images,
-        batch_size = batch_size,
-        save_to_dir = save_to_dir,
-        save_prefix  = image_save_prefix,
-        seed = seed)
+        batch_size=batch_size,
+        save_to_dir=save_to_dir,
+        save_prefix=image_save_prefix,
+        seed=seed)
 
     mask_generator = mask_datagen.flow(
         masks,
-        batch_size = batch_size,
-        save_to_dir = save_to_dir,
-        save_prefix  = image_save_prefix,
-        seed = seed)
+        batch_size=batch_size,
+        save_to_dir=save_to_dir,
+        save_prefix=image_save_prefix,
+        seed=seed)
 
     for (img, mask) in zip(image_generator, mask_generator):
         yield (img, mask)
@@ -151,8 +150,9 @@ def test_generator(test, target_size=(256,256), flag_multi_class=False, as_gray=
         yield img
 
 if __name__ == '__main__':
+    testing = False
     batch_size = 4
-    epochs = 150
+    epochs = 20
     data_aug = True
     steps_per_epoch = 20
     target_size = (256, 256)
@@ -166,7 +166,7 @@ if __name__ == '__main__':
 
     weights_path = f'unet_b_{batch_size}_e_{epochs}_s_{steps_per_epoch}_t_{target_size[0]}x{target_size[1]}.hdf5'
     loaded_weights = False
-    if not os.path.exists(weights_path):
+    if not os.path.exists(weights_path) or not testing:
         if len(os.listdir('data/resized_input')) <= 1:
             print('Loading scans')
             start = time.time()
@@ -175,6 +175,7 @@ if __name__ == '__main__':
         else:
             print('Loading input images')
             X_train = load_img('data/resized_input')
+            X_train = X_train / max_14b
 
         if len(os.listdir('data/resized_masks')) <= 1:
             print('Loading masks')
@@ -184,6 +185,8 @@ if __name__ == '__main__':
         else:
             print('Loading mask images')
             y_train = load_img('data/resized_masks')
+            y_train = y_train / max_16b
+
     else:
         print('Loading saved weights')
         loaded_weights = True
@@ -192,12 +195,14 @@ if __name__ == '__main__':
     X_test = [dicom.dcmread('data/test/test3_25/0_0.1_425_25.0_0.1_1.0_1.0_2.0_deformed.dcm').pixel_array,
               dicom.dcmread('data/test/test3_25/0_0.2_425_25.0_0.1_1.0_1.0_2.0_deformed.dcm').pixel_array,
               dicom.dcmread('data/test/test4_50/1_0.1_850_50.0_0.01_1.0_1.0_4.0_deformed.dcm').pixel_array,
-              dicom.dcmread('data/test/test4_50/1_0.2_850_50.0_0.01_1.0_1.0_4.0_deformed.dcm').pixel_array]
+              dicom.dcmread('data/test/test4_50/1_0.2_850_50.0_0.01_1.0_1.0_4.0_deformed.dcm').pixel_array
+             ]
 
     y_test = [dicom.dcmread('data/test/test3_25/0_0.1_425_25.0_0.1_1.0_1.0_2.0_deformed_mask.dcm').pixel_array,
               dicom.dcmread('data/test/test3_25/0_0.2_425_25.0_0.1_1.0_1.0_2.0_deformed_mask.dcm').pixel_array,
               dicom.dcmread('data/test/test4_50/1_0.1_850_50.0_0.01_1.0_1.0_4.0_deformed_mask.dcm').pixel_array,
-              dicom.dcmread('data/test/test4_50/1_0.2_850_50.0_0.01_1.0_1.0_4.0_deformed_mask.dcm').pixel_array]
+              dicom.dcmread('data/test/test4_50/1_0.2_850_50.0_0.01_1.0_1.0_4.0_deformed_mask.dcm').pixel_array
+             ]
 
     for i in range(len(X_test)):
         X_test[i] = X_test[i] / max_14b
@@ -215,21 +220,22 @@ if __name__ == '__main__':
     y_test = np.array(y_test)
     y_test = np.reshape(y_test, y_test.shape + (1,))
 
-    gc.collect() # collect unused memory. hopefully.
+    #gc.collect() # collect unused memory. hopefully.
 
     model = unet(input_size=target_size+(1,))
 
     model.summary()
 
-    if loaded_weights:
+    if loaded_weights and testing:
         model.load_weights(weights_path)
     else:
-        model_checkpoint = ModelCheckpoint(weights_path, monitor='loss',verbose=1, save_best_only=True)
+        model_checkpoint = ModelCheckpoint(weights_path, monitor='loss', verbose=1, save_best_only=True)
         #callbacks = []
         callbacks = [model_checkpoint]
-        train_gene = train_generator(X_train, y_train, batch_size=batch_size, target_size=target_size,
-                                     aug_dict=data_gen_args, save_to_dir=None)
-        model.fit_generator(train_gene, steps_per_epoch=steps_per_epoch, epochs=epochs, callbacks=callbacks)
+        train_gen = train_generator(X_train, y_train, batch_size=batch_size, target_size=target_size,
+                                    aug_dict=data_gen_args, save_to_dir=None)
+        model.fit_generator(train_gen, steps_per_epoch=len(X_train) / batch_size, epochs=epochs, callbacks=callbacks)
+        #model.fit(X_train, y_train, callbacks=callbacks, epochs=epochs, batch_size=batch_size)
 
     lr_scheduler = LearningRateScheduler(lr_sch)
     lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-6)
@@ -246,6 +252,6 @@ if __name__ == '__main__':
 
     i = 0
     for p in predict:
-        show_img(p, method='cv2')
-        show_img(y_test[i], method='cv2')
+        save_img((p * max_16b).astype(np.uint16), f'{i}.png', method='cv2')
+        #show_img(y_test[i], method='cv2')
         i += 1
