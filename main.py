84210e9b4198a05a4a88fa1f5c947b32df8df4c1
Jordy Vieira <jordyvieira@hotmail.com>
2020-04-29 21:31:38 -0300

    Fix background crop

--- main.py
+++ main.py
@@ -11,23 +11,28 @@ import time
 max_14b = float((2 ** 14) - 1)
 max_16b = float((2 ** 16) - 1)
 
-def crop_bg(img, gauss=True):
-    normm = normalize(img, True)
-    
-    edge = Image.fromarray(normm, 'L')
-    if gauss:
-        edge = edge.filter(ImageFilter.GaussianBlur)
-    edge = edge.filter(ImageFilter.FIND_EDGES)
-    
-    x, y = edge.size
-    edge = edge.crop((1,1,x-1,y-1))
-    edge = np.array(edge)
-    ret_val, thresh = cv2.threshold(edge, thresh=10, maxval=255, type=cv2.THRESH_BINARY)
-    points = np.argwhere(thresh!=0)
-    points = np.fliplr(points)
-    x, y, w, h = cv2.boundingRect(points)
-    ret = img[y:y+h, x:x+w]
-    return ret
+edges = {}
+
+def crop_bg(img, key, gauss=True, is_mask=False):
+    if not is_mask:
+        edge = Image.fromarray((img * 255).astype(np.uint8), 'L')
+        if gauss:
+            edge = edge.filter(ImageFilter.GaussianBlur)
+        edge = edge.filter(ImageFilter.FIND_EDGES)
+
+        x, y = edge.size
+        edge = edge.crop((1,1,x-1,y-1))
+        edge = np.array(edge)
+        ret_val, thresh = cv2.threshold(edge, thresh=10, maxval=255, type=cv2.THRESH_BINARY)
+        points = np.argwhere(thresh!=0)
+        points = np.fliplr(points)
+        x, y, w, h = cv2.boundingRect(points)
+        edges[key] = (x, y, w, h)
+        ret = img[y:y+h, x:x+w]
+        return ret
+    else:
+        x, y, w, h = edges[key]
+        return img[y:y+h, x:x+w]
 
 def normalize(img, int8=False, max_val=None):
     img = img / np.max(img) if max_val is None else img / max_val
@@ -68,7 +73,7 @@ def save_img(img, path, method='pillow', norm=False):
 
 def load_scans(path, target_size, is_mask=False, save=None):
     scans = []
-    for root, _, files in os.walk(path):
+    for root, _, files in sorted(os.walk(path)):
         for f in files:
             if f.find('.dcm') != -1:
                 scans.append(f'{root}/{f}')
@@ -76,13 +81,18 @@ def load_scans(path, target_size, is_mask=False, save=None):
     ret = []
     for s in scans:
         aux = dicom.dcmread(s).pixel_array
+        if is_mask:
+            key = s.lstrip('data/masks/')
+            key = key.replace('_mask', '')
+        else:
+            key = s.lstrip('data/input/')
 
         aux = aux / max_14b
-        #aux = crop_bg(aux)
+        aux = crop_bg(aux, key, is_mask=is_mask)
 
         if is_mask:
-            aux = invert(aux)
-            thresh = 0.61
+            #aux = invert(aux)
+            thresh = 0.42
             binarize(aux, thresh)
 
         aux = cv2.resize(aux, target_size, interpolation=cv2.INTER_AREA)
@@ -110,7 +120,7 @@ def load_scans(path, target_size, is_mask=False, save=None):
     
 def load_img(path, format='.png', is_mask=False):
     scans = []
-    for root, _, files in os.walk(path):
+    for root, _, files in sorted(os.walk(path)):
         for f in files:
             if f.find(format) != -1:
                 scans.append(np.array(cv2.imread(os.path.join(root, f), -1)))
@@ -152,7 +162,7 @@ def test_generator(test, target_size=(256,256), flag_multi_class=False, as_gray=
 if __name__ == '__main__':
     testing = False
     batch_size = 4
-    epochs = 20
+    epochs = 5
     data_aug = True
     steps_per_epoch = 20
     target_size = (256, 256)
@@ -164,23 +174,23 @@ if __name__ == '__main__':
                          horizontal_flip=True,
                          fill_mode='nearest')
 
-    weights_path = f'unet_b_{batch_size}_e_{epochs}_s_{steps_per_epoch}_t_{target_size[0]}x{target_size[1]}.hdf5'
+    weights_path = f'unet_b_{batch_size}_e_{epochs}_t_{target_size[0]}x{target_size[1]}.hdf5'
     loaded_weights = False
     if not os.path.exists(weights_path) or not testing:
-        if len(os.listdir('data/resized_input')) <= 1:
+        if len(os.listdir('data/resized_input/01')) <= 1:
             print('Loading scans')
             start = time.time()
-            X_train = load_scans('data/input', target_size, save=('data/resized_input', 'png'))
+            X_train = load_scans('data/input/01', target_size, save=('data/resized_input/01', 'png'))
             print(f'Took {time.time() - start}s to complete')
         else:
             print('Loading input images')
             X_train = load_img('data/resized_input')
             X_train = X_train / max_14b
 
-        if len(os.listdir('data/resized_masks')) <= 1:
+        if len(os.listdir('data/resized_masks/01')) <= 1:
             print('Loading masks')
             start = time.time()
-            y_train = load_scans('data/masks', target_size, is_mask=True, save=('data/resized_masks', 'png'))
+            y_train = load_scans('data/masks/01', target_size, is_mask=True, save=('data/resized_masks/01', 'png'))
             print(f'Took {time.time() - start}s to complete')
         else:
             print('Loading mask images')
@@ -192,28 +202,34 @@ if __name__ == '__main__':
         loaded_weights = True
 
     #X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2)
-    X_test = [dicom.dcmread('data/test/test3_25/0_0.1_425_25.0_0.1_1.0_1.0_2.0_deformed.dcm').pixel_array,
-              dicom.dcmread('data/test/test3_25/0_0.2_425_25.0_0.1_1.0_1.0_2.0_deformed.dcm').pixel_array,
-              dicom.dcmread('data/test/test4_50/1_0.1_850_50.0_0.01_1.0_1.0_4.0_deformed.dcm').pixel_array,
-              dicom.dcmread('data/test/test4_50/1_0.2_850_50.0_0.01_1.0_1.0_4.0_deformed.dcm').pixel_array
-             ]
+    x_paths = ['data/test/test3_25/0_0.1_425_25.0_0.1_1.0_1.0_2.0_deformed.dcm',
+              #'data/test/test3_25/0_0.2_425_25.0_0.1_1.0_1.0_2.0_deformed.dcm',
+              'data/test/test4_50/1_0.1_850_50.0_0.01_1.0_1.0_4.0_deformed.dcm',
+              #'data/test/test4_50/1_0.2_850_50.0_0.01_1.0_1.0_4.0_deformed.dcm'
+              ]
 
-    y_test = [dicom.dcmread('data/test/test3_25/0_0.1_425_25.0_0.1_1.0_1.0_2.0_deformed_mask.dcm').pixel_array,
-              dicom.dcmread('data/test/test3_25/0_0.2_425_25.0_0.1_1.0_1.0_2.0_deformed_mask.dcm').pixel_array,
-              dicom.dcmread('data/test/test4_50/1_0.1_850_50.0_0.01_1.0_1.0_4.0_deformed_mask.dcm').pixel_array,
-              dicom.dcmread('data/test/test4_50/1_0.2_850_50.0_0.01_1.0_1.0_4.0_deformed_mask.dcm').pixel_array
-             ]
+    y_paths = ['data/test/test3_25/0_0.1_425_25.0_0.1_1.0_1.0_2.0_deformed_mask.dcm',
+              #'data/test/test3_25/0_0.2_425_25.0_0.1_1.0_1.0_2.0_deformed_mask.dcm',
+              'data/test/test4_50/1_0.1_850_50.0_0.01_1.0_1.0_4.0_deformed_mask.dcm',
+              #'data/test/test4_50/1_0.2_850_50.0_0.01_1.0_1.0_4.0_deformed_mask.dcm'
+              ]
+    X_test = []
+    y_test = []
 
-    for i in range(len(X_test)):
-        X_test[i] = X_test[i] / max_14b
-        X_test[i] = cv2.resize(X_test[i], target_size, interpolation=cv2.INTER_AREA)
+    for path in x_paths:
+        aux = dicom.dcmread(path).pixel_array / max_14b
+        aux = crop_bg(aux, path.lstrip('data/test/'), is_mask=False)
+        aux = cv2.resize(aux, target_size, interpolation=cv2.INTER_AREA)
+        X_test.append(aux)
 
-    for i in range(len(y_test)):
-        y_test[i] = y_test[i] / max_14b
-        y_test[i] = invert(y_test[i])
-        thresh = 0.61
-        binarize(y_test[i], thresh)
-        y_test[i] = cv2.resize(y_test[i], target_size, interpolation=cv2.INTER_AREA)
+    for path in y_paths:
+        aux = dicom.dcmread(path).pixel_array / max_14b
+        aux = crop_bg(aux, path.lstrip('data/test/').replace('_mask', ''), is_mask=True)
+        #aux = invert(aux)
+        thresh = 0.42
+        binarize(aux, thresh)
+        aux = cv2.resize(aux, target_size, interpolation=cv2.INTER_AREA)
+        y_test.append(aux)
 
     X_test = np.array(X_test)
     X_test = np.reshape(X_test, X_test.shape + (1,))
